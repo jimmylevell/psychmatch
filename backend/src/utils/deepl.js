74d1cfa5d49304f623f8d8190ff2b05f53ae4dc0
James <jimmy.levell@bluewin.ch>
2022-05-21 18:13:59 +0200

    changed deepl communication to post

--- backend/src/utils/deepl.js
+++ backend/src/utils/deepl.js
@@ -9,10 +9,18 @@ const deepl = {};
 deepl.translate = function translate(text, sourceLang, targetLang) {
   const API =  config.deepl.API
   const auth_key = config.deepl.KEY
+  const body = 'auth_key=' + auth_key + '&text=' + text + '&target_lang=' + targetLang + '&source_lang=' + sourceLang
 
   console.log("Info: Translating text from " + sourceLang + " to " + targetLang);
 
-  return fetch(API + '?auth_key=' + auth_key + '&text=' + text + '&target_lang=' + targetLang + '&source_lang=' + sourceLang)
+  return fetch(API, {
+    headers: {
+      'Content-Type' : 'application/x-www-form-urlencoded',
+      'Content-Length': body.length
+    },
+    method: 'POST',
+    body: body
+  })
   .then(res => {
     return res.json()
   })
